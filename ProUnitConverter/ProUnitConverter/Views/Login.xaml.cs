5ebceb7dadf11cc759cd0c1d7c003694d9c3beaf
Aarush@122015 <you@example.com>
2024-04-15 06:41:09 +0530

    Adding Logs using log4Net

---
+++ ProUnitConverter/ProUnitConverter/Views/Login.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProUnitConverter.Views
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public Login()
+        {
+            InitializeComponent();
+
+        log.Info("Login Window Loaded.");
+        }
+}
+}
